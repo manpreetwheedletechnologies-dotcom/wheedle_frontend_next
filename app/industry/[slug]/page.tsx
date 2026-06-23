96d17b2f98e9f4e631fead52d85f236ecd7069f1
manpreetwheedletechnologies-dotcom <manpreet.wheedletechnologies@gmail.com>
2026-06-23 14:48:07 +0530

    added the new pages for industry

--- app/industry/[slug]/page.tsx
+++ app/industry/[slug]/page.tsx
@@ -1,7 +1,7 @@
 // src/app/industry/[slug]/page.tsx
 import { redirect } from "next/navigation";
 import { getIndustryBySlug, getAllIndustrySlugs } from "../../../lib/industries";
-
+import dynamic from 'next/dynamic';
 import Header from "../../../components/Header";
 import HeroSection from "../../../components/HeroSection";
 import HowWeHelpSection from "../../../components/Howwehelpsection";
@@ -36,8 +36,18 @@ export default function IndustryPage({ params }: { params: { slug: string } }) {
     const heroImage = industryHeroImages[slug.toLowerCase()];
     const solutionsImage = solutionsHeroImages[slug.toLowerCase()];
     const serveimage = serveImages[slug.toLowerCase()];
+    const AnimatedCursor = dynamic(() => import('react-animated-cursor'), {});
     return (
         <main className="w-full bg-[#0b0c10]">
+                    <AnimatedCursor
+          innerSize={8}
+          outerSize={30}
+          innerScale={1}
+          outerScale={2}
+          outerAlpha={0}
+          innerStyle={{ backgroundColor: '#FFFFFF' }}
+          outerStyle={{ backgroundColor: '#ffffff3b' }}
+        />
             <HeroSection data={industry.heroSection} heroImage={heroImage} />
             <HowWeHelpSection data={industry.servicesSection} />
             {industry.challengesSection && (
